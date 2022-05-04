e30114db16a13da09c3d5a5e50c2d733b885024e
mikelazquetax <azquetamikel@gmail.com>
2022-05-04 13:26:00 +0000

    Add App View

---
+++ mikelazqueta/webapp/controller/App.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller" 
+
+], function(Controller){
+
+    return Controller.extend("mikelazqueta.mikelazqueta.controller.App", {
+
+        onInit: function(){
+            
+        }
+
+
+    })
+
+})
